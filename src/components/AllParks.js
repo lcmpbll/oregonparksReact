7354a308f53cc0e47415c9e1d1ac0fb297df5b82
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-08 13:02:46 -0700

    BugFix: else statement for all parks encompassed the return statement.

--- src/components/AllParks.js
+++ src/components/AllParks.js
@@ -1,17 +1,17 @@
 import React, {useState, useEffect } from 'react';
 import ParksList from './ParksList';
-import ParkDetail from './ParkDetail';
+import ParkDetails from './ParkDetails';
 function AllParks() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [allParks, setAllParks] = useState([]);
   const [selectedPark, setSelectedPark] = useState(null);
- 
-  console.log(selectedPark);
+  const [selectedId, setSelectedId] = useState(null);
+  
+  console.log(selectedPark + "I am the selected park");
 
   useEffect(() => {
-   
-    fetch(`http://localhost:5000/api/v1/Parks/All`)
+    fetch(`http://localhost:5000/api/v2/Parks/All`)
         .then(response => {
           console.log(response);
           if(!response.ok) {
@@ -34,12 +34,14 @@ function AllParks() {
     const handleClick = () => {
       if(selectedPark != null) {
         setSelectedPark(null);
+        setSelectedId(null);
       }
     }
 
     const handleChangingSelectedPark = (parkId) => {
-      const selection = allParks.filter((park) => park.parkId === parkId)[0];
-           
+      const selection = allParks.filter(park => park.parkId === parkId)[0];
+      setSelectedId(parkId);
+      console.log(selection);
       setSelectedPark(selection);
     }
   const pageStyle = {
@@ -47,23 +49,26 @@ function AllParks() {
   }
    let currentlyVisibleState = null;
    let buttonText = "Enter"; 
+  
   if(error) {
     currentlyVisibleState = <h1>Error: {error}</h1>;
   } else if(!isLoaded) {
     currentlyVisibleState = <h1>...Loading...</h1>;
-  } else if (selectedPark != null) {
-    currentlyVisibleState = <ParkDetail park={selectedPark} />
+  } else if(selectedPark !== null){
+    currentlyVisibleState = <ParkDetails park={selectedPark} />
+    buttonText = "Return to all parks";
   } else { 
     currentlyVisibleState = <ParksList parkList={allParks} onParkSelection={handleChangingSelectedPark} />
-    return (
-      <React.Fragment>
-        <div style={pageStyle}>
-          {currentlyVisibleState}
-          {/* {error ? null : <button onClick={handleClick}>{buttonText}</button>} */}
-        </div>
-      </React.Fragment>
-    );
   }
+  return (
+    <React.Fragment>
+      <div style={pageStyle}>
+        {currentlyVisibleState}
+        {<button onClick={handleClick}>{buttonText}</button>}
+      </div>
+    </React.Fragment>
+  );
+  
 }
 
 export default AllParks;
